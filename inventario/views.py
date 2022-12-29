b51a644395b4011d96821024934cd5b7e1cfd9a9
_BL.Init10 <57692778+blinit10@users.noreply.github.com>
2022-12-29 14:20:30 -0500

    Fix shit

--- inventario/views.py
+++ inventario/views.py
@@ -14,11 +14,16 @@ def actualizar_sistema(request):
         response = requests.get('http://127.0.0.1:9000/backend/inventario/', headers=headers)
     else:
         headers = {'Authorization': 'Token 07e3d4a91f7098ad03ab59eede7f5f29a2728a20'}
-        response = requests.get('{}/backend/inventario/'.format(DatosOrganizacion.objects.all()[0].server), headers=headers)
+        response = requests.get('{}/backend/inventario/'.format(DatosOrganizacion.objects.all()[0].server),
+                                headers=headers)
     json_products = response.json()
     actual_products = []
     proveedores = ProveedorPropio.objects.values_list('nombre')
     for json_product in json_products['data']:
+        nombre = json_product['nombre_versat']
+        if nombre == 'None':
+            nombre = json_product['nombre']
+        print(nombre)
         if (str(json_product['proveedor']['nombre']),) in proveedores:
             locations = ""
             for location in json_product['municipios']:
@@ -26,7 +31,8 @@ def actualizar_sistema(request):
                     locations = locations + ", " + str(location['nombre'])
                 else:
                     locations = str(location['nombre'])
-            product = Producto(sku=json_product['sku'] or json_product['slug'], nombre=json_product['nombre_versat'],
+            product = Producto(sku=json_product['sku'] or json_product['slug'],
+                               nombre=nombre,
                                precio=json_product['precio'],
                                precio_lb=json_product['precioxlibra'], um=json_product['um'] or 'No especificada',
                                proveedor=json_product['proveedor']['nombre'], marca=json_product['marca']['nombre'],
